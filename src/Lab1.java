d2721200ca84c3e352bc5e101c32bcdac9f97c0f
Dylan <Dylan@10.134.227.53>
2019-01-16 09:16:07 -0700

    Lab 1 - Hello 335 Program

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,6 +1,6 @@
 /**
  * 
- * @author YOUR NAME HERE
+ * @author Dylan Fox
  * 
  * 
  */
@@ -8,7 +8,8 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
-		System.out.println("Hello, World!");
+		System.out.println("Hello CSC 335, I'm Dylan Fox");
+		return;
 	}
 
 }
